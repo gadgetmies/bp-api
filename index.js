c9b199a1f30cb5cf10bd32c61df6a2f6349d5bb8
Miko Kiiski <miko.kiiski@gmail.com>
2017-12-30 12:51:07 +0200

    Implement add to cart and get cart contents features

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const BPromise = require('bluebird')
-
+const R = require('ramda')
 const { init } = require('request-in-session')
 
 const loginUri = 'https://www.beatport.com/account/login'
@@ -14,14 +14,37 @@ const api = {
 
       const api = {
         getMyBeatport: callback => session.getJson('https://www.beatport.com/api/my-beatport', callback),
+        getMyBeatportTracks: (page, callback) => 
+          session.get(`https://www.beatport.com/my-beatport?page=${page}&_pjax=%23pjax-inner-wrapper`, (err, res) => {
+            if (err) {
+              return callback(err)
+            }
+          
+            const startString = 'window.Playables = '
+            const start = res.indexOf('window.Playables = ') + startString.length
+            const stop = res.indexOf(';', start)
+            callback(null, JSON.parse(res.substring(start, stop)))
+          }),
+        getItemsInCarts: (callback) => session.getJson('https://www.beatport.com/api/cart/cart', (err, res) => {
+          if (err) {
+            return callback(err)
+          }
+
+          const getJsonAsync = BPromise.promisify(session.getJson)
+          BPromise.map(res.carts.map(R.prop('id')), 
+            cartId => getJsonAsync(`https://www.beatport.com/api/cart/${cartId}`))
+            .map(({items}) => R.pluck('id', items))
+            .then(R.flatten)
+            .tap(idsOfItemsInCart => callback(null, idsOfItemsInCart))
+            .catch(err => callback(err))
+        }),
         getTrack: (trackId, callback) => session.getJson(`https://embed.beatport.com/track?id=${trackId}`, callback),
         getClip: (trackId, callback) => api.getTrack(trackId, (err, res) =>
           err ? callback(err) : callback(null, res.results.preview)),
-        addTrackToMainCart: (trackId, callback) => session.postJson(('https://www.beatport.com/api/cart', {
+        addTrackToCart: (trackId, cartId, callback) => session.postJson(`https://www.beatport.com/api/${cartId}`, {
           'items': [{ 'type': 'track', 'id': trackId }]
           // , "trackingData": { "type": "product", "id": "9915168", "name": "Contradictions", "position": "1", "brand": "Shogun Audio", "category": "Tracks", "variant": "track", "list": "Track Detail", "price": "1.56", "dimension1": "Alix Perez", "dimension2": null, "dimension3": "Drum & Bass", "dimension4": null, "dimension12": null } 
-        }), callback)
-
+        }, (err, res) => err ? callback(err) : callback(null, res))
       }
       return callback(null, api)
     })
