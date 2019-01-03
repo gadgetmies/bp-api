84f3f0dc850a2cf165df0364850d0f914b88d968
Miko Kiiski <miko.kiiski@reaktor.fi>
2019-01-03 16:11:25 +0200

    Implement support for initiating with csrf token and session cookie values

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const BPromise = require('bluebird')
 const R = require('ramda')
-const { init } = require('request-in-session')
+const { init, initWithSession } = require('request-in-session')
 
 const loginUri = 'https://www.beatport.com/account/login'
 const cookieUri = 'https://www.beatport.com/'
+const csrfTokenKey = '_csrf_token'
+const sessionCookieKey = 'session'
 
 const handleErrorOrCallFn = R.curry((errorHandler, fn) => (err, res) => err ? errorHandler(err) : fn(res))
 
@@ -15,69 +17,81 @@ const scrapeJSON = R.curry((startString, stopString, string) => {
 
 const getPlayables = pageSource => scrapeJSON('window.Playables = ', ';', pageSource)
 
-const api = {
-  init: (username, password, callback) => {
-    return init(cookieUri, loginUri, username, password, '_csrf_token', 'session', (err, session) => {
-      if (err) {
-        return callback(err)
-      }
-
-      const getJsonAsync = BPromise.promisify(session.getJson)
-      const beatportUri = `https://www.beatport.com`
-      const api = {
-        getMyBeatport: callback => session.getJson(`${beatportUri}/api/my-beatport`, callback),
-        getMyBeatportTracks: (page, callback) =>
-          session.get(`${beatportUri}/my-beatport?page=${page}&_pjax=%23pjax-inner-wrapper`,
-            handleErrorOrCallFn(callback, res => callback(null, getPlayables(res)))),
-        getItemsInCarts: (callback) => session.getJson(`${beatportUri}/api/cart/cart`,
-          handleErrorOrCallFn(callback, res => {
-            BPromise.map(res.carts.map(R.prop('id')),
-              cartId => getJsonAsync(`${beatportUri}/api/cart/${cartId}`))
-              .map(({ items }) => R.pluck('id', items))
-              .then(R.flatten)
-              .tap(idsOfItemsInCart => callback(null, idsOfItemsInCart))
-              .catch(err => callback(err))
-          })),
-        getTrack: (trackId, callback) => session.getJson(`https://embed.beatport.com/track?id=${trackId}`, callback),
-        getClip: (trackId, callback) => api.getTrack(trackId,
-          handleErrorOrCallFn(callback, res => callback(null, res.results.preview))),
-        addTrackToCart: (trackId, cartId, callback) => session.postJson(`${beatportUri}/api/${cartId}`, {
-          'items': [{ 'type': 'track', 'id': trackId }]
-        }, handleErrorOrCallFn(callback, res => callback(null, res))),
-        removeTrackFromCart: (trackId, cartId, callback) => session.deleteJson(`${beatportUri}/api/cart/${cartId}`, {
-          'items': [{ 'type': 'track', 'id': trackId }]
-        }, handleErrorOrCallFn(callback, res => callback(null, res))),
-        getAvailableDownloadIds: callback => session.get(`${beatportUri}/downloads/available?per-page=1000`,
-          handleErrorOrCallFn(callback, res => {
-            return BPromise.resolve(res)
-              .then(getPlayables)
-              .then(R.prop('tracks'))
-              .then(R.pluck('downloadId'))
-              .tap(downloadIds => callback(null, downloadIds))
-              .catch(err => callback(err))
-          })
-        ),
-        downloadTrackWithId: (downloadId, callback) =>
-          getJsonAsync(`${beatportUri}/api/downloads/purchase?downloadId=${downloadId}`)
-            .then(R.prop('download_url'))
-            .then(downloadUrl => session.getBlob(downloadUrl, callback))
-            .catch(err => callback(err))
-      }
-
-      const ensureLoginSuccessful = () => api.getMyBeatport(err => {
-        if (err) {
-          callback(err)
-        } else {
-          callback(null, api)
-        }
+const getApi = session => {
+  const getJsonAsync = BPromise.promisify(session.getJson)
+  const beatportUri = 'https://www.beatport.com'
+  const api = {
+    getMyBeatport: callback => session.getJson(`${beatportUri}/api/my-beatport`, callback),
+    getMyBeatportTracks: (page, callback) =>
+      session.get(`${beatportUri}/my-beatport?page=${page}&_pjax=%23pjax-inner-wrapper`,
+        handleErrorOrCallFn(callback, res => callback(null, getPlayables(res)))),
+    getItemsInCarts: (callback) => session.getJson(`${beatportUri}/api/cart/cart`,
+      handleErrorOrCallFn(callback, res => {
+        BPromise.map(res.carts.map(R.prop('id')),
+          cartId => getJsonAsync(`${beatportUri}/api/cart/${cartId}`))
+          .map(({ items }) => R.pluck('id', items))
+          .then(R.flatten)
+          .tap(idsOfItemsInCart => callback(null, idsOfItemsInCart))
+          .catch(err => callback(err))
+      })),
+    getTrack: (trackId, callback) => session.getJson(`https://embed.beatport.com/track?id=${trackId}`, callback),
+    getClip: (trackId, callback) => api.getTrack(trackId,
+      handleErrorOrCallFn(callback, res => callback(null, res.results.preview))),
+    addTrackToCart: (trackId, cartId, callback) => session.postJson(`${beatportUri}/api/${cartId}`, {
+      'items': [{ 'type': 'track', 'id': trackId }]
+    }, handleErrorOrCallFn(callback, res => callback(null, res))),
+    removeTrackFromCart: (trackId, cartId, callback) => session.deleteJson(`${beatportUri}/api/cart/${cartId}`, {
+      'items': [{ 'type': 'track', 'id': trackId }]
+    }, handleErrorOrCallFn(callback, res => callback(null, res))),
+    getAvailableDownloadIds: callback => session.get(`${beatportUri}/downloads/available?per-page=1000`,
+      handleErrorOrCallFn(callback, res => {
+        return BPromise.resolve(res)
+          .then(getPlayables)
+          .then(R.prop('tracks'))
+          .then(R.pluck('downloadId'))
+          .tap(downloadIds => callback(null, downloadIds))
+          .catch(err => callback(err))
       })
+    ),
+    downloadTrackWithId: (downloadId, callback) =>
+      getJsonAsync(`${beatportUri}/api/downloads/purchase?downloadId=${downloadId}`)
+        .then(R.prop('download_url'))
+        .then(downloadUrl => session.getBlob(downloadUrl, callback))
+        .catch(err => callback(err))
+  }
 
-      return ensureLoginSuccessful()
-    })
+  return api
+}
+
+const handleCreateSessionResponse = callback => (err, session) => {
+  if (err) {
+    return callback(err)
+  }
+  const api = getApi(session)
+  const ensureLoginSuccessful = () => api.getMyBeatport(err => {
+    if (err) {
+      callback(err)
+    } else {
+      callback(null, api)
+    }
+  })
+
+  return ensureLoginSuccessful()
+}
+
+const initializers = {
+  init: (username, password, callback) => {
+    return init(cookieUri, loginUri, username, password, csrfTokenKey, sessionCookieKey, handleCreateSessionResponse(callback))
+  },
+  initWithSession: (sessionCookieValue, csrfToken, callback) => {
+    return initWithSession(sessionCookieKey, sessionCookieValue, cookieUri, csrfTokenKey, csrfToken, handleCreateSessionResponse(callback))
   },
   initAsync: (username, password) =>
-    BPromise.promisify(api.init)(username, password)
+    BPromise.promisify(initializers.init)(username, password)
+      .then(api => BPromise.promisifyAll(api)),
+  initWithSessionAsync: (sessionCookieValue, csrfToken) =>
+    BPromise.promisify(initializers.initWithSession)(sessionCookieValue, csrfToken)
       .then(api => BPromise.promisifyAll(api))
 }
 
-module.exports = api
+module.exports = initializers
